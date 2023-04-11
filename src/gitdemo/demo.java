cc2c4a7a6a6a48c0f6cc36252476389b20846efa
arthicibi <arthisanthi1989@gmail.com>
2023-04-11 12:45:56 +0530

    project structure

--- src/gitdemo/demo.java
+++ src/gitdemo/demo.java
@@ -6,6 +6,8 @@ public class demo {
 		System.out.println("demo 1");
 		System.out.println("demo 2");
 		System.out.println("demo 3");
+		System.out.println("demo 4");
+		System.out.println("demo 5");
 
 	}
 }
